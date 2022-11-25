39e0f579db561d6ce99964cfa6b78cd2ada25e0d
Alek Novkovski <alek@greatcode2.com>
2022-11-26 00:32:25 +0100

    Delete Scrimba instructions

--- index.js
+++ index.js
@@ -35,16 +35,6 @@ function getFeedHtml(){
     `
     // document.body.innerHTML += markup
     }
-    /*
-    Challenge:
-    1. Use a "for of" to iterate over the data and
-       create HTML string for each tweet using the
-       boilerplate below. Replace UPPERCASE text
-       with data from the tweets.
-    2. Store this HTML in a let called "feedHtml".
-    3. Log out feedHtml.
-    4. Call getFeedHtml to check it's working.
-    */
     return markup
 }
 
