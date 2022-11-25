90770b2dff03fdd6cb37ab85b0e524061f0d11c9
Alek Novkovski <alek@greatcode2.com>
2022-11-25 23:20:59 +0100

    Refactor index.js to match Scrimba

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-/*
-Challenge:
-1. Put all of the data in its own file called
-   data.js, and export it back into index.js. 
-   Make any changes to index.html that are
-   necessary to make this work.
-2. Log out tweetsData.
-*/
-
-import data from './data'
-console.log(data)
+import { tweetsData } from './data.js'
