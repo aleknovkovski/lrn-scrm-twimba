8d4c0b8e193beb0b51ba9b0450df9c73e3437219
Alek Novkovski <alek@greatcode2.com>
2022-11-26 00:02:33 +0100

    Console.log tweet

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ import { tweetsData } from './data.js'
 
 const tweetInput = document.getElementById('tweet-input')
 const tweetBtn = document.getElementById('tweet-btn')
+
+tweetBtn.addEventListener("click", e=> {
+    console.log(tweetInput.value)
+})
